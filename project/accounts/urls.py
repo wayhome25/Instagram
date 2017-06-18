60b3c886c724318d518ad0661cde963154bc04fd
wayhome25 <siwabada@gmail.com>
2017-06-18 11:29:45 +0900

    로그인 기능 구현

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login', kwargs={'template_name': 'accounts/login.html'}),
 ]
