80bdb447142a590b877692caa06259f7c99f1525
wayhome25 <siwabada@gmail.com>
2017-06-18 11:56:05 +0900

    accounts.models.Profile 모델 생성 - 어드민 등록 - django.contrib.auth.models.User 모델과 1:1 관계 형성

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'user']
+    list_display_links = ['nickname', 'user']
+    search_fields = ['nickname']
--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.nickname
