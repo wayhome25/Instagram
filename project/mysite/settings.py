5a064a7c3a55ec3340740f32f9c43a447c5699fe
wayhome25 <siwabada@gmail.com>
2017-06-19 23:10:16 +0900

    post 앱 생성 - settings.py 앱 등록 - url include

--- project/mysite/settings.py
+++ project/mysite/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'imagekit',
     'accounts',
     'dojo',
-    'imagekit',
+    'post',
 ]
 
 MIDDLEWARE = [
--- project/mysite/urls.py
+++ project/mysite/urls.py
@@ -22,9 +22,10 @@ from django.shortcuts import redirect
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^dojo/', include('dojo.urls')),
+    url(r'^dojo/', include('dojo.urls', namespace='dojo')),
+    url(r'post/', include('post.urls', namespace='post')),
     url(r'^$', lambda r: redirect('profile'), name='root'), # NOTE: lambda 함수를 통한 리다이렉트
 ]
 
-# NOTE: serving media files uploaded by a user during development 
+# NOTE: serving media files uploaded by a user during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
