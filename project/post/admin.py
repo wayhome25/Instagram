05ba669ebdeb4e9e32fa47e5bac5631f64d9e738
wayhome25 <siwabada@gmail.com>
2017-06-19 23:48:00 +0900

    blog 앱에 Post, Tag 모델 생성 - admin 등록

--- project/post/admin.py
+++ project/post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'content', 'created_at']
+    list_display_links = ['author', 'content']
--- project/post/models.py
+++ project/post/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    photo = ProcessedImageField(upload_to='post/post/%Y/%m/%d',
+                                processors=[ResizeToFill(600, 600)],
+                                format='JPEG',
+                                options={'quality': 90})
+    content = models.CharField(max_length=140, help_text="최대 140자 입력 가능")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.name
