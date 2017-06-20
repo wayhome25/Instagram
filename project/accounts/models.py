8d538a685e26cda58cef4a932e8cfcf4295e5a41
wayhome25 <siwabada@gmail.com>
2017-06-20 18:48:53 +0900

    settings.AUTH_USER_MODEL 올바른 지정

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') # NOTE: User에서 접근시 활용 (user.profile.nickname)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
     nickname = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
