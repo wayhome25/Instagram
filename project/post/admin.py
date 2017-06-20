dead1b3b9717c19fe9b238b302735e7c0d31c6f0
wayhome25 <siwabada@gmail.com>
2017-06-20 12:23:16 +0900

    post.models.post 모델의 content 필드 위젯변경 Textarea 필드를 갖도록 forms.py, admin.py 수정

--- project/post/admin.py
+++ project/post/admin.py
@@ -1,11 +1,22 @@
+from django import forms
+from django.forms import Textarea
 from django.contrib import admin
 from .models import Post, Tag
 
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'content', 'created_at']
     list_display_links = ['author', 'content']
+    form = PostForm
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
--- project/post/forms.py
+++ project/post/forms.py
@@ -3,6 +3,13 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    photo = forms.ImageField(required=False)
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': 5,
+        'cols': 50,
+        'placeholder': '140자 까지 등록 가능합니다.\n#태그명 을 통해서 검색 태그를 등록할 수 있습니다. \n예시 : I #love #coding!',}))
+
+
     class Meta:
         model = Post
         fields = ['photo', 'content']
