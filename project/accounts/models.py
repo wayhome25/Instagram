6b88660239a8d0aa9000299f6dfe88e99bdfc5a1
wayhome25 <siwabada@gmail.com>
2017-06-28 21:00:39 +0900

    팔로우 기능 추가 (follow 뷰 구현)

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -6,13 +6,19 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=30, unique=True)
-    follow_set = models.ManyToManyField('self', through='Relation', symmetrical=False)
+    follow_set = models.ManyToManyField('self',
+                                        blank=True,
+                                        through='Relation',
+                                        symmetrical=False,)
 
     def __str__(self):
         return self.nickname
 
 
 class Relation(models.Model):
-    from_user = models.ForeignKey(Profile, related_name='follow_relations')
-    to_user = models.ForeignKey(Profile, related_name='follower_relations')
+    from_user = models.ForeignKey(Profile, related_name='follower_user')
+    to_user = models.ForeignKey(Profile, related_name='follow_user')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} -> {}".format(self.from_user, self.to_user)
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -2,9 +2,9 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import SignupForm
-from .models import Profile
+from .models import Profile, Relation
 
 def signup(request):
     if request.method == 'POST':
@@ -40,3 +40,16 @@ def password_change(request):
     return render(request, 'accounts/password_change.html', {
         'form': form,
     })
+
+@login_required
+def follow(request, pk):
+    from_user = request.user.profile
+    to_user = get_object_or_404(Profile, pk=pk)
+    relation, created = Relation.objects.get_or_create(from_user=from_user, to_user=to_user)
+
+    if created:
+        messages.success(request, '팔로우 시작!')
+    else:
+        relation.delete()
+        messages.success(request, '팔로우 취소')
+    return redirect('post:post_list')
