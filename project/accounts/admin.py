381340ab35215e9ceb8d1db92b479dd57a2b21d6
wayhome25 <siwabada@gmail.com>
2017-06-28 18:10:53 +0900

    Profile 모델에 follow_set 필드 추가, Relation 모델 생성 - admin 등록

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Relation
+
+
+class FollowInline(admin.TabularInline):
+    model = Relation
+    fk_name = 'from_user'
 
 
 @admin.register(Profile)
@@ -7,3 +12,10 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id', 'nickname', 'user']
     list_display_links = ['nickname', 'user']
     search_fields = ['nickname']
+    inlines = [FollowInline, ]
+
+
+@admin.register(Relation)
+class RelationAdmin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'created_at']
+    list_display_links = ['from_user', 'to_user', 'created_at']
--- project/accounts/models.py
+++ project/accounts/models.py
@@ -4,8 +4,15 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=30, unique=True)
+    follow_set = models.ManyToManyField('self', through='Relation', symmetrical=False)
 
     def __str__(self):
         return self.nickname
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(Profile, related_name='follow_relations')
+    to_user = models.ForeignKey(Profile, related_name='follower_relations')
+    created_at = models.DateTimeField(auto_now_add=True)
