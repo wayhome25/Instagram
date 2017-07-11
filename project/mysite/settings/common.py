7a79d3c18467b0a8a965effa63208815be2cd4b7
wayhome25 <siwabada@gmail.com>
2017-07-11 17:42:44 +0900

    SECRET_KEY 를 비밀파일(json)으로 분리

--- project/mysite/settings/common.py
+++ project/mysite/settings/common.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
+import os, json
 from os.path import abspath, dirname, join
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,8 +21,28 @@ BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["INSTA_SECRET_KEY"]
-# SECRET_KEY = 'b_4(!id8ro!1645n@ubnbvf1hbu93gaia0^_o%2$#hw-@but!v'
+# 환경변수 패턴
+# SECRET_KEY = os.environ["INSTA_SECRET_KEY"]
+
+# 비밀파일 패턴을 활용한 SECRET_KEY 가져오기
+from django.core.exceptions import ImproperlyConfigured
+
+# JSON 기반 비밀 모듈
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # 배포용 prod.py 에서 오버라이딩
