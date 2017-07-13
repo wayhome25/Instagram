d20eaa173dc3ff9f7ded72504f17c595d4a90b96
wayhome25 <siwabada@gmail.com>
2017-07-13 15:28:17 +0900

    django-extensions Graph models 사용을 위한 설정 추가

--- project/mysite/settings/dev.py
+++ project/mysite/settings/dev.py
@@ -5,3 +5,9 @@ INSTALLED_APPS += ['debug_toolbar',]
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
 
 INTERNAL_IPS = ["127.0.0.1"] # NOTE: djanog_debug_toolbar 용 설정 추가
+
+# Graph models 설정
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
